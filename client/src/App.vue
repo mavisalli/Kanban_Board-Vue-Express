4924be15b884008be743fec13019e49f9c8e3f45
mavi <mavi.baris96@gmail.com>
2021-12-25 02:29:18 +0300

    Save visited pages to localstorage

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,7 +5,21 @@
 </template>
 
 <script>
-export default {};
+export default {
+  created() {
+    //save visited pages to localstorage
+    const path = this.$route.path;
+    const backlistPaths = ["/"];
+
+    if (backlistPaths.includes(path)) return;
+
+    const paths = JSON.parse(localStorage.getItem("paths")) || [];
+
+    paths.push(path);
+
+    localStorage.setItem("paths", JSON.stringify(paths));
+  },
+};
 </script>
 
 <style></style>
