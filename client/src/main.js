db147e580705ed5f39b8ec6f4376903b2291ec63
mavi <mavi.baris96@gmail.com>
2021-12-24 16:24:04 +0300

    Do config operations

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import "./assets/css/bootstrap.min.css";
+import "./assets/css/style.css";
+import VueRouter from "vue-router";
+import { routes } from "./routes";
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+Vue.config.productionTip = false;
+
+const router = new VueRouter({
+  mode: "history",
+  routes,
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+  router,
+}).$mount("#app");
