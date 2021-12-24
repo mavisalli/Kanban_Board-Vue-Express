4d2f0d657aee63b42e4ab5abaa291665b8c3205e
mavi <mavi.baris96@gmail.com>
2021-12-24 16:12:34 +0300

    Create a Task List component to use inside the board page

---
+++ client/src/components/Tasklist.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="tasklist-item" :class="title.toLowerCase().replace(' ', '-')">
+    <h3 class="tasklist-item-title">
+      {{ title }}
+    </h3>
+    <draggable
+      class="list-group"
+      :list="items"
+      group="cards"
+      @change="changed(column_id, $event)"
+    >
+      <card v-for="item in items" :key="item._id" :data="item" />
+    </draggable>
+  </div>
+</template>
+<script>
+import draggable from "vuedraggable";
+import Card from "./Card.vue";
+import axios from "axios";
+
+export default {
+  props: ["items", "title", "column_id"],
+  components: {
+    Card,
+    draggable,
+  },
+  methods: {
+    // changed: async (column_id, evt) => {
+    //   if (!evt) return;
+    //   if (evt.added) {
+    //     let card_id = evt.added.element._id;
+    //     await axios.post("http://localhost:3000/addToColumn", {
+    //       card_id,
+    //       column_id,
+    //     });
+    //   } else if (evt.removed) {
+    //     let card_id = evt.removed.element._id;
+    //     await axios.post("http://localhost:3000/removeFromColumn", {
+    //       card_id,
+    //       column_id,
+    //     });
+    //   } else if (evt.moved) {
+    //     let cards = this.items.map((item) => ({
+    //       _id: item._id,
+    //     }));
+    //     await axios.post("http://localhost:3000/updateCardOrder", {
+    //       cards,
+    //       column_id,
+    //     });
+    //   }
+    // },
+    changed: function (column_id, evt) {
+      if (!evt) return;
+      if (evt.added) {
+        let card_id = evt.added.element._id;
+        axios
+          .post("http://localhost:3000/addToColumn", { card_id, column_id })
+          .then((res) => {
+            console.log(res.data);
+          });
+      } else if (evt.removed) {
+        let card_id = evt.removed.element._id;
+        axios
+          .post("http://localhost:3000/removeFromColumn", {
+            card_id,
+            column_id,
+          })
+          .then((res) => {
+            console.log(res.data);
+          });
+      } else if (evt.moved) {
+        const cards = this.items.map((item) => ({
+          _id: item._id,
+        }));
+        axios
+          .post("http://localhost:3000/updateCardOrder", {
+            column_id,
+            cards,
+          })
+          .then((res) => {
+            console.log(res.data);
+          });
+      }
+    },
+  },
+  async created() {
+    await this.changed();
+  },
+};
+</script>
+
+<style scoped>
+.backlog .card-item {
+  background-color: gray;
+}
+
+.to-do .card-item {
+  background-color: #d93535;
+}
+
+.in-progress .card-item {
+  background-color: orange;
+}
+
+.done .card-item {
+  background-color: #00a88b;
+}
+
+.tasklist-item {
+  background: #262626;
+  border-radius: 16px;
+  padding: 24px;
+}
+
+.tasklist-item .tasklist-item-title {
+  font-weight: 700;
+  font-size: 37px;
+  padding: 24px 24px 16px 24px;
+  margin: 16px 0px;
+  color: #ffffff;
+}
+</style>
