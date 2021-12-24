200da715b6821112dce3d5b20555d6d1fd9321d1
mavi <mavi.baris96@gmail.com>
2021-12-24 16:36:02 +0300

    Update Home component

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -4,7 +4,7 @@
       Hello, you can go to the board I created by typing
       http://localhost:3000/61c5bbd43f049ea09ee4bd0c in the url.
     </h3>
-    <h4>You can find other boards below.</h4>
+    <h4>You can find other boards below:</h4>
 
     <p>61c4c618d3fe5e020fd31fe5</p>
     <p>61c3d3e2ac04b7c543d8621d</p>
@@ -17,21 +17,21 @@ export default {};
 
 <style scoped>
 h3 {
-  font-size: 30px;
-  font-weight: 700;
-  color: #ffffff;
-  text-align: center;
-}
-
-h4 {
   font-size: 25px;
   font-weight: 700;
   color: #ffffff;
   text-align: center;
 }
 
-p {
+h4 {
   font-size: 20px;
+  font-weight: 700;
+  color: #ffffff;
+  text-align: center;
+}
+
+p {
+  font-size: 15px;
   font-weight: 400;
   color: #ffffff;
   text-align: center;
