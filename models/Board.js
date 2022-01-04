3d4224655275a725b15be1745050d3433a21b385
mavi <mavi.baris96@gmail.com>
2022-01-04 23:48:56 +0300

    move under server folder

--- models/Board.js
+++
@@ -1,8 +0,0 @@
-const Mongoose = require("mongoose");
-
-const BoardSchema = new Mongoose.Schema({
-  title: { type: String, required: true },
-  columns: [{ type: Mongoose.Schema.Types.ObjectId, ref: "Column" }],
-});
-
-module.exports = Mongoose.model("Board", BoardSchema);
