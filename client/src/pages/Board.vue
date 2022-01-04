e101432911ecb4a7b59ac8b372c3d84c30ccaf06
mavi <mavi.baris96@gmail.com>
2022-01-04 23:44:35 +0300

    create card add input

--- client/src/pages/Board.vue
+++ client/src/pages/Board.vue
@@ -4,6 +4,21 @@
       <div class="col-12 board-title">
         {{ board.result.title }}
       </div>
+
+      <div class="form">
+        <label>Card Title :</label>
+        <input type="input" v-model="new_card.title" placeholder=" required" />
+
+        <label>Card Text :</label>
+        <input
+          type="input"
+          v-model="new_card.description"
+          placeholder=" required"
+        />
+
+        <button @click="createNewCard" class="button">Add Card</button>
+      </div>
+
       <div
         class="col-12 col-md-3"
         v-for="(item, index) in board.result.columns"
@@ -36,6 +51,10 @@ export default {
         },
       },
       error: null,
+      new_card: {
+        title: "",
+        description: "",
+      },
     };
   },
   components: {
@@ -43,13 +62,29 @@ export default {
   },
   async created() {
     try {
-      const id = this.$route.params.id;
-      this.board = (
-        await axios.get(`http://localhost:3000/boards?board_id=${id}`)
-      ).data;
+      const slug = this.$route.params.slug;
+      this.board = (await axios.get(`boards?board_title=${slug}/`)).data;
     } catch (error) {
       this.error = error;
     }
+  },
+
+  methods: {
+    createNewCard() {
+      axios
+        .post("cards/", {
+          title: this.new_card.title,
+          description: this.new_card.description,
+          column_id: this.board.result.columns[0]._id,
+        })
+        .then((res) => {
+          const newCard = res.data;
+
+          this.board.result.columns[0].cards.push(newCard.card);
+          this.new_card.title = "";
+          this.new_card.description = "";
+        });
+    },
   },
 };
 </script>
@@ -61,6 +96,7 @@ export default {
   color: #ffffff;
   text-align: center;
   padding-bottom: 20px;
+  margin-top: -90px;
 }
 
 .col-12 {
@@ -72,5 +108,38 @@ h3 {
   font-size: 30px;
   font-weight: 700;
   text-align: center;
+}
+
+input {
+  width: 20%;
+  padding: 5px 10px;
+  margin: 2px 10px;
+  border: solid 1px;
+  background-color: rgb(30, 31, 30);
+  color: aliceblue;
+}
+
+.button {
+  background-color: #e67e22;
+  border: none;
+  color: white;
+  width: 10%;
+  font-size: 17px;
+  font-weight: 600;
+  cursor: pointer;
+}
+
+label {
+  color: #6e6b69;
+  font-size: 20px;
+  font-weight: 600;
+  margin-top: 3px;
+}
+
+.form {
+  display: flex;
+  justify-content: center;
+  margin-top: -10px;
+  margin-bottom: 50px;
 }
 </style>
